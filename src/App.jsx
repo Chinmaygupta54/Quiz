1ac0e3e395a06e2e98db3afc6a161395f3738013
Jayesh027 <jayeshdarwani27@gmail.com>
2023-08-15 21:59:18 +0530

    updated app.jsx
    inserted header , footer , loginForm in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
+import { BrowserRouter} from "react-router-dom"
+import Footer from "./component/footer"
+import Header from "./component/header"
+import LoginForm from "./component/loginForm"
+
 export const App = () => {
-
-
-  
+    return(
+        <BrowserRouter>
+          <Header />
+          <Footer />
+          <LoginForm />    
+        </BrowserRouter>
+    )
 }
