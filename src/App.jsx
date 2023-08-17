08ba778e60e060455d91c6a9950fcefa371ad5cd
VeeruSinghShekhawat <veerusing156@gmail.com>
2023-08-17 23:01:50 +0530

    Css change in login and new sign in form

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import { BrowserRouter} from "react-router-dom"
 import Footer from "./component/footer"
 import Header from "./component/header"
 import LoginForm from "./component/loginForm"
+import SignIn from "./component/signForm"
 
 export const App = () => {
     return(
         <BrowserRouter>
           <Header />
           <Footer />
-          <LoginForm />    
+          <LoginForm />   
+          < SignIn />
         </BrowserRouter>
     )
 }
