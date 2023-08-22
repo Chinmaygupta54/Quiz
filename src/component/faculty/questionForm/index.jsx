63323e483ccb408e7aa10b243d2f05ae6e7f0e3d
Jayesh027 <jayeshdarwani27@gmail.com>
2023-08-22 10:37:41 +0530

    validations in question form

--- src/component/faculty/questionForm/index.jsx
+++ src/component/faculty/questionForm/index.jsx
@@ -16,6 +16,13 @@ const QustionForm = ({addQuestion}) => {
         navigate("/questionList")
     }
     const add = () => {
+        if(question.length ===0||optionA.length === 0||optionB.length === 0||optionC.length === 0||optionD.length === 0){
+            alert("Please Enter question and their options")
+        }
+        else if(correctAnswer.length === 0){
+            alert("Please select correct option")
+        }
+        else{
         addQuestion({
             question : question,
             optionA : optionA,
@@ -31,44 +38,50 @@ const QustionForm = ({addQuestion}) => {
         setOptionD('')
         setCorrectAnswer('')
     }
+}
 
   return (
     <Wrapper>
             <form action = "">
               <h1>Add Questions</h1>
-                <input
+                <input 
                     type ='textarea'
                     placeholder = "Enter Question"
                     value = {question}
                     onChange = {(e) => setQuestion(e.target.value)} 
+                    
                 />
                 <input
                     type = "text"
                     placeholder = "option A"
                     value = {optionA}
-                    onChange = {(e) => setOptionA(e.target.value)} 
+                    onChange = {(e) => setOptionA(e.target.value)}
+                     
                 />
                 <input
                     type = "text"
                     placeholder = "option B"
                     value = {optionB}
                     onChange = {(e) => setOptionB(e.target.value)} 
+                    
                 />
                 <input
                     type = "text"
                     placeholder = "option C"
                     value = {optionC}
                     onChange = {(e) => setOptionC(e.target.value)} 
+                    
                 />
                 <input
                     type = "text"
                     placeholder = "option D"
                     value = {optionD}
                     onChange = {(e) => setOptionD(e.target.value)} 
+                    
                 />
                 <select name = "type"
                     onChange = {(e) => setCorrectAnswer(e.target.value)}
-                    required
+                    
                 >
 			        <option value selected disabled >--Select correct option--</option>
 			        <option value = "optionA">option A</option>
